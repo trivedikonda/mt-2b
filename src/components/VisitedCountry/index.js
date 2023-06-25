d8d284f26c5e5f7ef587d3913a78b900068e495a
TRIVEDI <120240029+trivedikonda@users.noreply.github.com>
2023-06-25 19:20:02 +0530

    Update index.js

--- src/components/VisitedCountry/index.js
+++ src/components/VisitedCountry/index.js
@@ -1,26 +1,22 @@
-import {Component} from 'react'
-
-class VisitedCountry extends Component {
-  render() {
-    const {visitedCountryDetails, removeCountry} = this.props
-    // console.log(visitedCountryDetails)
-    const {name, imageUrl, id} = visitedCountryDetails
-    return (
-      <li>
-        <img width={200} height={100} src={imageUrl} alt="thumbnail" />
-        <div>
-          <p>{name}</p>
-          <button
-            type="button"
-            onClick={() => {
-              removeCountry(id)
-            }}
-          >
-            Remove
-          </button>
-        </div>
-      </li>
-    )
-  }
+const VisitedCountry = props => {
+  const {visitedCountryDetails, removeCountry} = props
+  const {name, imageUrl, id} = visitedCountryDetails
+  return (
+    <li>
+      <img width={200} height={100} src={imageUrl} alt="thumbnail" />
+      <div>
+        <p>{name}</p>
+        <button
+          type="button"
+          onClick={() => {
+            removeCountry(id)
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    </li>
+  )
 }
+
 export default VisitedCountry
